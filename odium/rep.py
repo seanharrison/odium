806984a183d956a3e911bf9e5c9166d730775b42
Sean Harrison <sah@bookgenesis.com>
2018-06-16 14:21:20 -0500

    basic async json REQ client and REP server

---
+++ odium/rep.py
@@ -0,0 +1,37 @@
+"""
+provides a REP endpoint listening for REQ.
+Each REQ connection is handled by a coroutine (asyncio) and REP sent when its work is done.
+"""
+import logging
+import random
+import zmq.asyncio
+import asyncio
+import uvloop
+
+log = logging.getLogger(__name__)
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+def rep_server(port, req_processor):
+    context = zmq.asyncio.Context()
+    server = context.socket(zmq.REP)
+    server.bind(f"tcp://*:{port}")
+    loop = asyncio.get_event_loop()
+    loop.create_task(req_handler(server, req_processor))
+    loop.run_forever()
+
+async def req_handler(server, req_processor):
+    while True:
+        req = await server.recv_json()
+        rep = await req_processor(req)
+        server.send_json(rep, separators=(',',':'))
+
+async def echo(req):
+    log.info(req)
+    return req
+
+if __name__=='__main__':
+    import sys
+    from odium import config
+    logging.basicConfig(level=20)
+    port = config['ports']['REQ_REP']
+    rep_server(port, echo)
---
+++ odium/req.py
@@ -0,0 +1,30 @@
+
+import time
+import logging
+import zmq.asyncio
+import asyncio
+import uvloop
+from multiprocessing import Process
+from random import random
+
+log = logging.getLogger(__name__)
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+async def send_req(host, port, req):
+    context = zmq.asyncio.Context()
+    client = context.socket(zmq.REQ)
+    client.connect(f"tcp://{host}:{port}")
+    await client.send_json(req, separators=(',',':'))
+    rep = await client.recv_json()
+    return rep
+
+if __name__=='__main__':
+    from odium import config
+    import json, sys
+    logging.basicConfig(level=20)
+    host = 'localhost'
+    port = config['ports']['REQ_REP']
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(send_req(host, port, {'argv': sys.argv[1:]}))
+    rep = loop.run_until_complete(task)
+    log.info(json.dumps(rep, separators=(',',':')))
